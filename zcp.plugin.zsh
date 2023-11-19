1d22e42f1ca0c0a5a20822d79f66e4d8e5434d39
victor.litoux <victor.litoux@epita.fr>
2023-11-19 01:38:05 +0100

    Add first tqdm bar, need to check optimization

--- zcp.plugin.zsh
+++ zcp.plugin.zsh
@@ -1,6 +1,6 @@
 zcp() {
 
-    local SIZE_SOURCE_KILOBYTE=888 #$(du -s "$1" | awk '{print $1}')
+    local SIZE_SOURCE_KILOBYTE=$(du -S -b "$1" | cut -f1)
     local SOURCES=${@:1:#-1} # Remove the last argument
     local TARGET=${@:$#}
     local NUMBER_FILES=4 #$(ls -1 "$1" | wc -l)
@@ -9,36 +9,17 @@ zcp() {
     echo "NUMBER_FILES: $NUMBER_FILES"
     echo "SOURCES: $SOURCES"
     echo "TARGET: $TARGET"
-    strace -q \cp -r $SOURCES $TARGET 2>&1 | awk '{
-        # Check if "openat" is in the line
-        if (match($0, /openat/)) {
-            print "-----Begin-------"
-            print $0
-            if (match($0,/O_WRONLY/)) {
-                printf "openat write \n"
-            }
-            else if (match($0,/O_RDONLY/)) {
-                printf "openat read \n"
-            }
-            print "----count-----"
-            print $NF
-            print "------End-------"
-        }
+    strace -q \cp -r $@ 2>&1 | awk '{
         #Check if "write in the line
-        else if (match($0, /write/)) {
-            print "-----Begin-------"
-            print $0
-            printf "simple write\n"
-            print "----count-----"
+        if (match($0, /write/)) {
             print $NF
-            print "------End-------"
         }
-    }'
-
+        sleep 1
+    }'| tqdm --update --null --unit_scale --unit-divisor=1024  --unit=B --total $SIZE_SOURCE_KILOBYTE 
 }
 
 RESOURCES="test_resources/"
-SOURCE="$RESOURCES""few_files/"
+SOURCE="$RESOURCES""huges_files/"
 TARGET="$RESOURCES""target/"
 rm -rf $TARGET
 mkdir $TARGET
